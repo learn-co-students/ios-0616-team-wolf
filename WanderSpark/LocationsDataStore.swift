c2970a561fe5a6127cca571218a9348860c84598
cklynch2 <cklynch2@gmail.com>
2016-08-09 15:21:51 -0400

    Location name, snippet and images now successfully processed in data parser rather than data store.

--- WanderSpark/LocationsDataStore.swift
+++ WanderSpark/LocationsDataStore.swift
@@ -26,59 +26,22 @@ class LocationsDataStore {
         
         while page < 60 {
             
-            print("PAGE: \(page)")
-            
             NYTimesAPIClient.getLocationsWithCompletion(page) { (thirtySixHoursArray) in
-                print("PAGE SENT TO NYTIMES API: \(page)")
+
+                let parser = NYTimesDataParser()
                 
                 for article in thirtySixHoursArray {
+                    let locationName = parser.getLocationName(article)
+                    let locationSnippet = parser.getLocationSnippet(article)
+                    let locationImages = parser.getLocationImages(article)
 
-                    article = NYTimesDataParser.
-                    
-                    
-//                    guard let
-//                        headline = article["headline"] as? [String:String],
-//                        keywords = article["keywords"] as? [[String:String]],
-//                        snippet = article["snippet"] as? String,
-//                        multimedia = article["multimedia"] as? [[String: AnyObject]]
-//                        else { print("Could not create location object from supplied dictionary."); return }
-//                    
-//                    // There are a variable number of keywords for each article. Iterate over the entire array in order to pull out the one that contains the location name. The location name is usually formatted as "City Name (State or Country)" so pull this one out by finding the value that contains parentheses substring.
-//                    var locationName = ""
-//                    for keyword in keywords {
-//                        guard let value = keyword["value"] else { print("Error: No value key in keywords dictionary."); return }
-//                        if value.containsString("(") {
-//                            locationName = value
-//                        }
-//                    }
-//                    
-//                    // Sometimes the keyword does not include (State or Country), in which case using the parentheses to find the location name does not work. In this case, pull the location name from the main headline.
-//                    if locationName == "" {
-//                        guard let mainHeadline = headline["main"] else { print("Error: No main key in headline dictionary."); return }
-//                        if mainHeadline.containsString("36 Hours in") {
-//                            locationName = mainHeadline.stringByReplacingOccurrencesOfString("36 Hours in ", withString: "")
-//                        } else if mainHeadline.containsString("36 Hours:") {
-//                            locationName = mainHeadline.stringByReplacingOccurrencesOfString("36 Hours: ", withString: "")
-//                        }
-//                    }
-//                    
-//                    var images = [String]()
-//                    for item in multimedia {
-//                        guard let mediaType = item["type"] as? String else { print("Error: No type key for media item."); return }
-//                        
-//                        if mediaType == "image" {
-//                            guard let imageURL = item["url"] as? String else { print("Error: Failure getting image url from multimedia array."); return }
-//                            images.append(imageURL)
-//                        }
-//                    }
-                    
                     print("Name: \(locationName)")
-                    print("Description: \(snippet)")
-                    print("Images: \(images)")
+                    print("Description: \(locationSnippet)")
+                    print("Images: \(locationImages)")
                     print("##############################")
                     
                     if locationName != "" {
-                        let location = Location(name: locationName, description: snippet, images: images)
+                        let location = Location(name: locationName, description: locationSnippet, images: locationImages)
                         self.locations.append(location)
                         print("Location count: \(self.locations.count)")
                     }
@@ -86,9 +49,7 @@ class LocationsDataStore {
             }
             page += 1  
         }
-        
-        print("COMPLETE")
         completion()
-        }
+    }
     
 }
