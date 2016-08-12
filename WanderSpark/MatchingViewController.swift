3bb23bc3303e030de4876d478dc9cbb40092f511
cklynch2 <cklynch2@gmail.com>
2016-08-12 10:53:33 -0400

    Match cards now only show one parameter. Parameter is added to negative match array for left swipe, positive match array for right swipe.

--- WanderSpark/MatchingViewController.swift
+++ WanderSpark/MatchingViewController.swift
@@ -15,17 +15,26 @@ class MatchingViewController: UIViewController {
     
     var matchingView: KolodaView!
     
-    var matchParameters = [String]()
+    var positiveMatchParameters = [String]()
+    var negativeMatchParameters = [String]()
 
-    let matchingKeys = [("City", "Country"),
-                        ("Mountains", "Beaches"),
-                        ("Shopping", "Outdoors"),
-                        ("Sightseeing", "Nightlife"),
-                        ("Historic", "Modern"),
-                        ("Food", "Fitness"),
-                        ("Luxury", "Adventure")]
+    let matchingKeys = ["City",
+                        "Country",
+                        "Mountains",
+                        "Beaches",
+                        "Shopping",
+                        "Outdoors",
+                        "Sightseeing",
+                        "Nightlife",
+                        "Historic",
+                        "Modern",
+                        "Food",
+                        "Fitness",
+                        "Luxury",
+                        "Adventure"]
     
-    let matchingIcons: [(UIImage, UIImage)] = [(cityImage, countryImage), (mountainsImage, beachesImage), (shoppingImage, outdoorsImage), (sightseeingImage, nightlifeImage), (historicImage, modernImage), (foodieImage, fitnessImage), (luxuryImage, adventureImage)]
+    let matchingIcons: [UIImage] = [cityImage, countryImage, mountainsImage, beachesImage, shoppingImage, outdoorsImage, sightseeingImage, nightlifeImage, historicImage, modernImage, foodieImage, fitnessImage, luxuryImage, adventureImage]
+
 //    let leftArrow: UIImage = leftArrowImage
 //    let rightArrow: UIImage = rightArrowImage
     
@@ -40,9 +49,8 @@ class MatchingViewController: UIViewController {
         self.matchingView.centerYAnchor.constraintEqualToAnchor(self.view.centerYAnchor).active = true
         self.matchingView.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor, multiplier: 0.85).active = true
         self.matchingView.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.75).active = true
-       
         
-         self.view.backgroundColor = UIColor.darkGrayColor()
+        self.view.backgroundColor = UIColor.darkGrayColor()
         
         matchingView.dataSource = self
         matchingView.delegate = self
@@ -52,7 +60,7 @@ class MatchingViewController: UIViewController {
 extension MatchingViewController: KolodaViewDelegate {
     func kolodaDidRunOutOfCards(matchingView: KolodaView) {
         // Initialize and run the LocationMatchmaker based on the matching parameters.
-        let matcher = LocationMatchmaker.init(matchParameters: matchParameters)
+        let matcher = LocationMatchmaker.init(matchParameters: positiveMatchParameters)
         matcher.tallyLocationMatches()
         matcher.sortLocationsByMatchCount()
         matcher.returnMatchedLocations()
@@ -68,17 +76,19 @@ extension MatchingViewController: KolodaViewDelegate {
     
     func koloda(matchingView: KolodaView, didSwipeCardAtIndex index: UInt, inDirection direction: SwipeResultDirection) {
         
+        let matchWord = matchingKeys[Int(index)]
+        
         if direction == .Left {
-            let matchWord = matchingKeys[Int(index)].1
-            matchParameters.append(matchWord)
+            negativeMatchParameters.append(matchWord)
             print("Left swipe : \(matchWord)")
             
         } else if direction == .Right {
-            let matchWord = matchingKeys[Int(index)].0
-            matchParameters.append(matchWord)
+            positiveMatchParameters.append(matchWord)
             print("Right swipe : \(matchWord)")
         }
-        print("These words have been added to the matching parameters array: \(matchParameters)")
+        
+        print("These words have been added to the positive matching parameters array: \(positiveMatchParameters)")
+        print("These words have been added to the negative matching parameters array: \(negativeMatchParameters)")
         // Does this also need to present the next card or is that automatically built into Koloda?
     }
 }
@@ -96,92 +106,38 @@ extension MatchingViewController: KolodaViewDataSource {
         
         matchingCardView.backgroundColor = UIColor.whiteColor()
         
-        // Add and constrain matching icons:
-        let iconViewOne = UIImageView()
-        let iconViewTwo = UIImageView()
+        // Add and constrain matching icon:
+        let iconView = UIImageView()
+        matchingCardView.addSubview(iconView)
         
-        matchingCardView.addSubview(iconViewOne)
-        matchingCardView.addSubview(iconViewTwo)
+        let icon = matchingIcons[Int(index)]
+        iconView.image = icon
+        iconView.contentMode = .ScaleAspectFill
         
-        let iconOne = matchingIcons[Int(index)].0
-        let iconTwo = matchingIcons[Int(index)].1
-        
-        iconViewOne.image = iconOne
-        iconViewTwo.image = iconTwo
-        
-        iconViewOne.contentMode = .ScaleAspectFill
-        iconViewTwo.contentMode = .ScaleAspectFill
-
-        iconViewOne.snp_makeConstraints { make in
-            make.left.equalTo(matchingCardView).inset(10)
+        iconView.snp_makeConstraints { make in
+            make.centerX.equalTo(matchingCardView)
             make.centerY.equalTo(matchingCardView).offset(-20)
-            make.width.height.equalTo(175)
+            make.width.height.equalTo(225)
         }
         
-        iconViewTwo.snp_makeConstraints { make in
-            make.right.equalTo(matchingCardView).inset(10)
-            make.centerY.equalTo(matchingCardView).offset(-20)
-            make.width.height.equalTo(175)
-        }
+        // Add and constrain matching label:
+        let matchWordLabel = UILabel()
+        matchingCardView.addSubview(matchWordLabel)
         
-        // Add and constrain matching labels:
-        let labelOne = UILabel()
-        let labelTwo = UILabel()
+        matchWordLabel.text = matchingKeys[Int(index)]
+        matchWordLabel.textAlignment = .Center
         
-        labelOne.text = matchingKeys[Int(index)].0
-        labelTwo.text = matchingKeys[Int(index)].1
-        
-        labelOne.textAlignment = .Center
-        labelTwo.textAlignment = .Center
-        
-        matchingCardView.addSubview(labelOne)
-        matchingCardView.addSubview(labelTwo)
-        
-        labelOne.snp_makeConstraints { make in
-            make.centerX.equalTo(iconViewOne)
-            make.top.equalTo(iconViewOne.snp_bottom).offset(20)
+        matchWordLabel.snp_makeConstraints { make in
+            make.centerX.equalTo(iconView)
+            make.top.equalTo(iconView.snp_bottom).offset(20)
             make.width.equalTo(100)
             make.height.equalTo(25)
         }
-        
-        labelTwo.snp_makeConstraints { make in
-            make.centerX.equalTo(iconViewTwo)
-            make.top.equalTo(iconViewTwo.snp_bottom).offset(20)
-            make.width.equalTo(100)
-            make.height.equalTo(25)
-        }
-        
-        // Add and constrain arrows:
-//        let arrowViewOne = UIImageView()
-//        let arrowViewTwo = UIImageView()
-//        
-//        matchingCardView.addSubview(arrowViewOne)
-//        matchingCardView.addSubview(arrowViewTwo)
-//        
-//        arrowViewOne.image = leftArrow
-//        arrowViewTwo.image = rightArrow
-//        
-//        arrowViewOne.contentMode = .ScaleAspectFit // Zain: I used Fit instead of Fill here, because it's not a square and may be harder to match the aspect ratio you use in Sketch to the aspect ratio in the constraints.
-//        arrowViewTwo.contentMode = .ScaleAspectFit
-//        
-//        arrowViewOne.snp_makeConstraints { make in
-//            make.centerX.equalTo(iconViewOne)
-//            make.top.equalTo(labelOne.snp_bottom).offset(20)
-//            make.width.equalTo(60)
-//            make.height.equalTo(25)
-//        }
-//        
-//        arrowViewTwo.snp_makeConstraints { make in
-//            make.centerX.equalTo(iconViewTwo)
-//            make.top.equalTo(labelTwo.snp_bottom).offset(20)
-//            make.width.equalTo(60)
-//            make.height.equalTo(25)
-//        }
         
         return matchingCardView
     }
     
-    // Do not know if we need this...
+
 //    func koloda(koloda: KolodaView, viewForCardOverlayAtIndex index: UInt) -> OverlayView? {
 //        return NSBundle.mainBundle().loadNibNamed("OverlayView",
 //                                                  owner: self, options: nil)[0] as? OverlayView
