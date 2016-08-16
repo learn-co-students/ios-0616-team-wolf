c8f1309cee367dd502c6f8665ee546f33cebc3ed
bmanjuu <bettyofung@gmail.com>
2016-08-16 10:55:23 -0400

    wrote function to match carrier id and information

--- WanderSpark/SkyScannerDataParser.swift
+++ WanderSpark/SkyScannerDataParser.swift
@@ -14,14 +14,33 @@ class SkyScannerDataParser {
     
     static let flightData = SkyScannerAPIClient.lowestPrices[0]
     static let flightCarrierID = flightData["CarrierIds"] as? [Int]
-    static let flightOrigin = flightData["OriginId"] as? Int
-    static let flightDestination = flightData["DestinationId"] as? Int //may not need this but adding it just in case 
+    static let flightOriginID = flightData["OriginId"] as? Int
+    static let flightDestinationID = flightData["DestinationId"] as? Int //may not need this but adding it just in case
     
-    let carrierInformation = SkyScannerAPIClient.carrierInformation
-    let locationInformation = SkyScannerAPIClient.locationInformation
+    static let flightCarrierName : String
+    static let flightOriginIATACode : String
+    static let flightOriginName : String
+    static let flightDestinationName : String
     
-    class func matchCarrierInformation(/*take in carrierInformation array? */) -> String {
-        
+    
+    class func matchCarrierInformation() -> String {
+        for carrier in SkyScannerAPIClient.carrierInformation {
+            if carrier["CarrierId"] as? Int == flightCarrierID {
+                flightCarrierName = carrier["Name"] as? String
+            }
+        }
+        return flightCarrierName
     }
+    
+    
+    class func matchLocationInformation() -> String /*(name: String, IATACode: String) */{
+        for originID in SkyScannerAPIClient.locationInformation {
+            
+        }
+        
+        return ""
+    }
+    
+    
     
 }
