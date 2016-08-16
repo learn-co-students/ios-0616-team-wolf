055b8b83ccb48d5bbbc8cde18616d0d49cd81a18
bmanjuu <bettyofung@gmail.com>
2016-08-16 10:48:30 -0400

    setting up data parser to match carrier id and information

--- WanderSpark/SkyScannerDataParser.swift
+++ WanderSpark/SkyScannerDataParser.swift
@@ -12,4 +12,16 @@ import Alamofire
 
 class SkyScannerDataParser {
     
+    static let flightData = SkyScannerAPIClient.lowestPrices[0]
+    static let flightCarrierID = flightData["CarrierIds"] as? [Int]
+    static let flightOrigin = flightData["OriginId"] as? Int
+    static let flightDestination = flightData["DestinationId"] as? Int //may not need this but adding it just in case 
+    
+    let carrierInformation = SkyScannerAPIClient.carrierInformation
+    let locationInformation = SkyScannerAPIClient.locationInformation
+    
+    class func matchCarrierInformation(/*take in carrierInformation array? */) -> String {
+        
+    }
+    
 }
