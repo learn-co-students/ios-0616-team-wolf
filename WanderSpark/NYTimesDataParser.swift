0fad86757a372c0644f9a5494199dcf46142efda
cklynch2 <cklynch2@gmail.com>
2016-08-18 12:06:33 -0400

    Edited NYTimesDataParser functions to return optionals and took out assertion failures.

--- WanderSpark/NYTimesDataParser.swift
+++ WanderSpark/NYTimesDataParser.swift
@@ -10,52 +10,58 @@ import Foundation
 
 struct NYTimesDataParser {
     
-    typealias ParsedString = (String, ErrorType?)
-    typealias ParsedArray = ([String], ErrorType?)
-    
-    static func initializeLocationsFromJSON(JSONArray: [[String : AnyObject]]) -> [Location] {
+    static func initializeLocationsFromJSON(JSONArray: [[String : AnyObject]]) -> [Location]? {
         
-        var unfilteredLocations = [Location]()
+        var unfilteredLocations : [Location]
         
         for article in JSONArray {
-            let locationName = NYTimesDataParser.getLocationName(article)
-            let locationSnippet = NYTimesDataParser.getLocationSnippet(article)
-            let locationImages = NYTimesDataParser.getLocationImages(article)
-            let articleURL = NYTimesDataParser.getArticleURL(article)
-            
-            print("###############################")
-            print("Name: \(locationName)")
-            print("Description: \(locationSnippet)")
-            print("Images: \(locationImages)")
-            
-            if locationName != "" && !locationImages.isEmpty {
+            if let locationName = NYTimesDataParser.getLocationName(article),
+            locationSnippet = NYTimesDataParser.getLocationSnippet(article),
+            locationImages = NYTimesDataParser.getLocationImages(article),
+            articleURL = NYTimesDataParser.getArticleURL(article) {
+                
+                print("###############################")
+                print("Name: \(locationName)")
+                print("Description: \(locationSnippet)")
+                print("Images: \(locationImages)")
+                
                 let location = Location(name: locationName, description: locationSnippet, images: locationImages, url: articleURL)
                 unfilteredLocations.append(location)
+                
+            } else {
+               print("Error: NYTimesDataParser")
             }
         }
         return unfilteredLocations
     }
     
     
-    static func getLocationName(article: [String : AnyObject]) -> String {
-        var locationName = ""
+    static func getLocationName(article: [String : AnyObject]) -> String? {
+        var locationName : String?
         
         guard let
             headline = article["headline"] as? [String:String],
             keywords = article["keywords"] as? [[String:String]]
-            else { assertionFailure("Could not get location keywords or headline from supplied dictionary."); return "" }
+            else { print("Error: No location keywords or headline from supplied dictionary.")
+                return nil }
         
         // The location name is usually formatted as "City Name (State or Country)" so pull this out by finding keyword with value that contains parentheses substring.
         for keyword in keywords {
-            guard let value = keyword["value"] else { assertionFailure("Error: No value key in keywords dictionary."); return "" }
-            if value.containsString("(") {
+            guard let value = keyword["value"] else { print("Error: No value key in keywords dictionary.")
+                return nil }
+            
+            if value.containsString("Civil War") {
+                locationName = "Gettysburg (PA)"
+            } else if value.containsString("(") {
                 locationName = value
             }
         }
         
         // Sometimes the keyword does not include (State or Country), so using parentheses to find  location name does not work. In this case, pull location name from the main headline.
-        if locationName == "" {
-            guard let mainHeadline = headline["main"] else { assertionFailure("Error: No main key in headline dictionary."); return "" }
+        if locationName == nil {
+            guard let mainHeadline = headline["main"] else { print("Error: No main key in headline dictionary.")
+                return nil }
+            
             if mainHeadline.containsString("36 Hours in") {
                 locationName = mainHeadline.stringByReplacingOccurrencesOfString("36 Hours in ", withString: "")
             } else if mainHeadline.containsString("36 Hours:") {
@@ -66,36 +72,49 @@ struct NYTimesDataParser {
     }
     
     
-    static func getLocationSnippet(article: [String : AnyObject]) -> String {
+    static func getLocationSnippet(article: [String : AnyObject]) -> String? {
         
-        guard let snippet = article["snippet"] as? String
-            else { assertionFailure("Could not get location snippet from supplied dictionary."); return "" }
-        return snippet
+        if let snippet = article["snippet"] as? String {
+            return snippet
+        } else {
+            print("Could not get location snippet from supplied dictionary.")
+            return nil
+        }
     }
     
     
-    static func getLocationImages(article: [String : AnyObject]) -> [String] {
+    static func getLocationImages(article: [String : AnyObject]) -> [String]? {
         
         guard let multimedia = article["multimedia"] as? [[String: AnyObject]]
-            else { assertionFailure("Could not get location multimedia from supplied dictionary."); return [] }
+            else { print("Could not get location multimedia from supplied dictionary.")
+                return nil }
         
-        var images = [String]()
+        // Replace for-loop with filter for items with type equal to image.
+        
         for item in multimedia {
-            guard let mediaType = item["type"] as? String else { assertionFailure("Error: No type key for media item."); return [] }
+            guard let mediaType = item["type"] as? String else { print("Error: No type key for media item.")
+                return nil }
             
             if mediaType == "image" {
-                guard let imageURL = item["url"] as? String else { assertionFailure("Error: Failure getting image url from multimedia array."); return [] }
-                images.append(imageURL)
+                if let imageURL = item["url"] as? String {
+                    images.append(imageURL)
+                } else {
+                    print("Error: Could not get image url from multimedia array.")
+                    return nil
+                }
             }
         }
         return images
     }
     
     
-    static func getArticleURL(article: [String: AnyObject]) -> String {
-        guard let url = article["web_url"] as? String
-            else { assertionFailure("Could not get article URL from supplied dictionary."); return "" }
-        return url
+    static func getArticleURL(article: [String: AnyObject]) -> String? {
+        if let url = article["web_url"] as? String {
+            return url
+        } else {
+            print("Error: Could not get article URL from supplied dictionary.")
+            return nil
+        }
     }
     
 }
